46f98962c33f9eca98642cb056b0fd55c4c27a6e
tsutsu1231 <220387@jc-21.jp>
2022-11-29 13:45:20 +0900

    2022-11-29

---
+++ kadai1/kadai1_add_check.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>課題１</title>
+    </head>
+    <body>
+        
+    <?php
+
+    //わかんないから飛ばす2022-11-22  p62
+
+    $kadai1_ID=$_POST['ID'];
+    $kadai1_name=$_POST['name'];
+    $kadai1_hurigana=$_POST['hurigana'];
+    $kadai1_yubin=$_POST['yubin'];
+    $kadai1_jusyo=$_POST['jusyo'];
+    $kadai1_denwa=$_POST['denwa'];
+    $kadai1_email=$_POST['email'];
+
+    $kadai1_ID=htmlspecialchars($kadai1_ID,ENT_QUOTES,'UTF-8');
+    $kadai1_name=htmlspecialchars($kadai1_name,ENT_QUOTES,'UTF-8');
+    $kadai1_hurigana=htmlspecialchars($kadai1_hurigana,ENT_QUOTES,'UTF-8');
+    $kadai1_yubin=htmlspecialchars($kadai1_yubin,ENT_QUOTES,'UTF-8');
+    $kadai1_jusyo=htmlspecialchars($kadai1_jusyo,ENT_QUOTES,'UTF-8');
+    $kadai1_denwa=htmlspecialchars($kadai1_denwa,ENT_QUOTES,'UTF-8');
+    $kadai1_email=htmlspecialchars($kadai1_email,ENT_QUOTES,'UTF-8');
+    
+
+    if($kadai1_ID=='')
+    {
+        print'IDが入力されていません。<br/>';
+    }
+    else
+    {
+        print'ID:';
+        print $kadai1_ID;
+        print'<br/>';
+    }
+
+    if($kadai1_name=='')
+    {
+        print'氏名が入力されていません。<br/>';
+    }
+    else
+    {
+        print'氏名:';
+        print $kadai1_name;
+        print'<br/>';
+    }
+
+    if($kadai1_hurigana=='')
+    {
+        print'ふりがなが入力されていません。<br/>';
+    }
+    else
+    {
+        print'ふりがな:';
+        print $kadai1_hurigana;
+        print'<br/>';
+    }
+
+    if($kadai1_yubin=='')
+    {
+        print'郵便番号が入力されていません。<br/>';
+    }
+    else
+    {
+        print'郵便番号:';
+        print $kadai1_yubin;
+        print'<br/>';
+    }
+
+    if($kadai1_jusyo=='')
+    {
+        print'住所が入力されていません。<br/>';
+    }
+    else
+    {
+        print'住所:';
+        print $kadai1_jusyo;
+        print'<br/>';
+    }
+
+    if($kadai1_denwa=='')
+    {
+        print'電話番号が入力されていません。<br/>';
+    }
+    else
+    {
+        print'電話番号:';
+        print $kadai1_denwa;
+        print'<br/>';
+    }
+
+    if($kadai1_name=='')
+    {
+        print'Emailが入力されていません。<br/>';
+    }
+    else
+    {
+        print 'Email:';
+        print $kadai1_email;
+        print'<br/>';
+    }
+
+    if($kadai1_ID==''|| $kadai1_name==''||$kadai1_hurigana=='' || $kadai1_yubin=='' || $kadai1_jusyo=='' || $kadai1_denwa=='' ||$kadai1_email=='')
+    {
+        print'<form>';
+        print'<input type="button" onclick="history.back()" value="戻る">';
+        print'</form>';
+    }
+    else
+    {
+        //$staff_pass=md5($staff_pass);  //わからない　p63
+        $kadai1_ID=md5($kadai1_ID);
+        $kadai1_name=md5($kadai1_name);
+        $kadai1_hurigana=md5($kadai1_hurigana);
+        $kadai1_yubin=md5($kadai1_yubin);
+        $kadai1_jusyo=md5($kadai1_jusyo);
+        $kadai1_denwa=md5($kadai1_denwa);
+        $kadai1_email=md5($kadai1_email);
+        print'<form method="post" action="kadai1_add_done.php">';
+        print'<input type="hidden" name="ID" value="'.$$kadai1_ID.'">';
+        print'<input type="hidden" name="name" value="'.$kadai1_name.'">';
+        print'<input type="hidden" name="hurigana" value="'.$$kadai1_hurigana.'">';
+        print'<input type="hidden" name="yubin" value="'.$$kadai1_yubin.'">';
+        print'<input type="hidden" name="jusyo" value="'.$$kadai1_jusyo.'">';
+        print'<input type="hidden" name="denwa" value="'.$$kadai1_denwa.'">';
+        print'<input type="hidden" name="email" value="'.$$kadai1_email.'">';
+        print'<br/>';
+        print'<input type="button" onclick="history.back()" value="戻る">';
+        print'<input type ="submit" value="OK">';
+        print'</from>';
+    }
+
+    ?>
+    </body>
+</html>
---
+++ kadai1/kadai1_list.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>課題１</title>
+    </head>
+    <body>
+
+    <?php
+    
+    try
+    {
+    
+        //p80
+
+    $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+    $user='root';
+    $password='';
+    $dbh=new PDO($dsn,$user,$password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    $sql='SELECT ID,name FROM kojin WHERE 1';
+    $stmt=$dbh->prepare($sql);
+    $stmt->execute();
+
+    $dbh=null;
+
+    print'一覧<br/><br/>';
+
+    print'<form method="post" action="kadai1_branch.php">';
+    while(true)
+    {
+        $rec=$stmt->fetch(PDO::FETCH_ASSOC);
+        if($rec==false)
+        {
+            break;
+        }
+        print'<input type="radio" name="staffcode" value="'.$rec['code'].'">';
+        print$rec['name'];
+        print'<br/>';
+    }
+    print'<input type="submit" name="edit" value="参照">';
+    print'<input type="submit" name="edit" value="追加">';
+    print'<input type="submit" name="edit" value="修正">';
+    print'<input type="submit" name="delete" value="削除">';
+    print'</form>';
+    }
+    catch(Exception $e)
+    {
+        print "ご迷惑をお掛けしています";
+    }
+    
+   
+    ?>
+    </body>
+</html>
