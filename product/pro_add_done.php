3cfe822f19a74279fdd34a79186f76ede13b0e2a
tsutsu1231 <220387@jc-21.jp>
2022-12-20 15:06:50 +0900

    2022-12-20-3

--- product/pro_add_done.php
+++ product/pro_add_done.php
@@ -13,6 +13,7 @@
         
         $pro_name=$_POST['name'];
         $pro_price=$_POST['price'];
+        $pro_gazou_name=$_POST['gazou_name'];
 
         $pro_name=htmlspecialchars($pro_name,ENT_QUOTES,'UTF-8');
         $pro_price=htmlspecialchars($pro_price,ENT_QUOTES,'UTF-8');
@@ -24,10 +25,11 @@
         $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
         print "<pre>";
-        $sql='INSERT INTO mst_product(name,price) VALUES(?,?)';
+        $sql='INSERT INTO mst_product(name,price,gazou) VALUES(?,?,?)';
         $stmt=$dbh->prepare($sql);
         $data[]=$pro_name;
         $data[]=$pro_price;
+        $data[]=$pro_gazou_name;
         $stmt->execute($data);
 
         $dbh=null;
