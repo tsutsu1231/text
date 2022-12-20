5deb3497f430948e3b2fcbafd54cc86ae6d17c26
tsutsu1231 <220387@jc-21.jp>
2022-12-20 14:39:31 +0900

    2022-12-20-2

--- product/pro_delete.php
+++ product/pro_delete.php
@@ -19,14 +19,13 @@ $password='';
 $dbh=new PDO($dsn,$user,$password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-$sql='SELECT name,price FROM mst_product WHERE code=?';
+$sql='SELECT name FROM mst_product WHERE code=?';
 $stmt=$dbh->prepare($sql);
 $data[]=$pro_code;
 $stmt->execute($data);
 
 $rec = $stmt->fetch(PDO::FETCH_ASSOC);
 $pro_name=$rec['name'];
-$pro_price=$rec['price'];
 
 $dbh=null;
 
@@ -39,21 +38,18 @@ catch(Exception $e)
 
 ?>
 
-スタッフ修正<br/>
+商品削除<br/>
 <br/>
-スタッフコード<br/>
-<?php print $staff_code;?>
+商品コード<br/>
+<?php print $pro_code;?>
 <br/>
+商品名<br/>
+<?php print $pro_name; ?>
 <br/>
-<form method="post" action="staff_edit_check.php">
-<input type="hidden" name="code" value="<?php print $staff_code;?>">
-スタッフ名<br/>
-<input type="text" name="name" style="width:200px" value=" <?php print $staff_name;?>"><br/>
-パスワードを入力してください。<br/>
-<input type="password" name="pass" style="width:100px"><br/>
-パスワードをもう一度入力してください。<br/>
-<input type="password" name="pass2" style="width:100px"><br/>
+この商品を削除してよろしいですか？<br/>
 <br/>
+<form method="post" action="pro_delete_done.php">
+<input type="hidden" name="code" value="<?php print $pro_code;?>">
 <input type ="button" onclick="history.back()" value="戻る">
 <input type="submit" value="OK">
 </form>
---
+++ product/pro_edit_done.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>ろくまる農園</title>
+    </head>
+    <body>
+
+        <?php
+        
+        try
+        {
+        
+        $pro_code=$_POST['code'];
+        $pro_name=$_POST['name'];
+        $pro_price=$_POST['price'];
+
+        $pro_code=htmlspecialchars($pro_code,ENT_QUOTES,'UTF-8');
+        $pro_name=htmlspecialchars($pro_name,ENT_QUOTES,'UTF-8');
+        $pro_price=htmlspecialchars($pro_price,ENT_QUOTES,'UTF-8');
+
+        $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+        $user='root';
+        $password='';
+        $dbh=new PDO($dsn,$user,$password);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+        print "<pre>";
+        $sql='UPDATE mst_product SET name=?,price=? WHERE code=?';
+        $stmt=$dbh->prepare($sql);
+        $data[]=$pro_name;
+        $data[]=$pro_price;
+        $data[]=$pro_code;
+        $stmt->execute($data);
+
+        $dbh=null;
+
+        print'修正しました。<br/>';
+
+        }
+        catch(Exception $e)
+        {
+            print'ただいま障害により大変ご迷惑をお掛けしております。';
+            print"<br>".$e->getMessage();
+            exit();
+        }
+        
+        ?>
+
+        <a href="pro_list.php">戻る</a>
+        
+    </body>
+</html>
