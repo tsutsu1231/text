c92e3fdad174f025e873272a0d8c9e413bab93d6
tsutsu1231 <220387@jc-21.jp>
2023-01-18 13:46:27 +0900

    2023-01-18

--- kadai2/kadai2_list.php
+++ kadai2/kadai2_list.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>課題2</title>
-        <link href="kadai2_add.css" rel="stylesheet" type="text/css">
+        <link href="kadai2_list.css" rel="stylesheet" type="text/css">
     </head>
     <body>
 
@@ -26,27 +26,41 @@
 
     print'画像一覧<br/><br/>';
 
-    print' ';
-    print'ID';
-    print' ';
-    print'タイトル';
-    print' ';
-    print'サムネイル';
-
     print'<form method="post" action="kadai2_branch.php">';
+
+    echo "<table>";
+
+    echo "<tr>";
+
+    print "<th> </th>";
+    print"<th>ID</th>";
+    //print' ';
+    print"<th>タイトル</th>";
+    //print' ';
+    print"<th>サムネイル</th>";
+
+    echo "</tr>";
+
     while(true)
     {
         $rec=$stmt->fetch(PDO::FETCH_ASSOC);
+
         if($rec==false)
         {
             break;
         }
-        print'<input type="radio" name="kadai2code" value="'.$rec['id'].'">';
-        print$rec['title'];
+        $kadai2_file_name=$rec['file'];
+        $list_file='<img src="./image/'.$kadai2_file_name.'">';
+        print'<td><input type="radio" name="kadai2code" value="'.$rec['id'].'">';
+        print"<td>".$rec['id']."</td>";
+        print"<td>".$rec['title']."</td>";
         //print$rec['description'];
         //print$rec['file'];
+        print "<td><div class=gazou>$list_file</div></td>";
+        print'<br/>';
         print'<br/>';
     }
+    echo "</table>";
     print'<input type="submit" name="disp" value="参照">';
     print'<input type="submit" name="add" value="追加">';
     //print'<input type="submit" name="edit" value="修正">';
